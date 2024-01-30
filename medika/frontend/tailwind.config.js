68e17dcd47da9b7e4e40c73e905d859b527af87a
VChiofalo <117775290+VChiofalo@users.noreply.github.com>
2024-01-30 12:03:54 +0100

    installation et configuration de tailwindcss (#2)

---
+++ medika/frontend/tailwind.config.js
@@ -0,0 +1,32 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'black': '#111816',
+        'white': '#EDEDED',
+        primary: '#E15100',
+        accentuary: '#E15100',
+        secondary: '#79BAF6'
+      },
+      fontFamily: {
+        'Satoshi': ['Satoshi', 'Courier New', 'Arial', 'Helvetica']
+      },
+      fontSize: {
+        sm: '9px',
+        base: '14px',
+        large: '18px',
+        xl: '20px',
+        '2xl': '25px',
+        '3xl': '52px', 
+        '4xl': '84px'
+      }
+    },
+  },
+  plugins: [],
+}
+
