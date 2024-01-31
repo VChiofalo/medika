c2be15be44aa9a02726b2c33f393f9155c4b5431
AlexFraioli <102045767+alexfrai@users.noreply.github.com>
2024-01-31 14:31:41 +0100

    Weight page + Comp TYPO merge vers le main (#13)
    * Commencement sur la page Poids
    
    * Page Poid aspect visuel + manque le graph + ajout du composant TYPO
    
    * Recup des donné avant push sur la main du comp TYPO et de la page poid (#12)
    
    * docker phpmyadmin (#9)
    
    ok docker php
    
    * modif nom fichier sql
    
    * changement nom fichier bdd
    
    * Mise en place back dotenv, .env, .env.example (#11)
    
    * Modif config tailwind
    
    * Installation de dotenv
    
    * Modif primary color tailwind
    
    * Modifs config tailwind
    
    * Delete medika/backend/.env
    
    * Ajout .env au gitignore
    
    * Ajout .env.example
    
    * Renomer entité Vaccine sur looping et fichier sql (#10)
    
    * docker phpmyadmin
    
    * Renomer entité Vaccine sur looping et fichier sql
    
    ---------
    
    Co-authored-by: Erwan <128072355+Veleoran@users.noreply.github.com>
    Co-authored-by: VChiofalo <117775290+VChiofalo@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Erwan <128072355+Veleoran@users.noreply.github.com>
    Co-authored-by: VChiofalo <117775290+VChiofalo@users.noreply.github.com>

--- medika/frontend/src/App.jsx
+++ medika/frontend/src/App.jsx
@@ -1,10 +1,14 @@
 import { useState } from 'react'
 import './App.css'
 import AnimalCard from './components/animal_card'
-import WeightCard from './components/weight_card'
+import WeightStatusCard from './components/weight_status_card'
 import VaccineCard from './components/vaccine_card'
 import Navbar from './components/navbar'
 import SupervisionCard from './components/supervision_card'
+import Header from './components/header'
+import GraphCard from './components/graph_card'
+import WeightCard from './components/weight_card'
+import IccCard from './components/icc_card'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,13 +17,21 @@ function App() {
     <>
       <AnimalCard/>
       <div className='flex justify-center'>
-        <WeightCard/>
+        <WeightStatusCard/>
         <VaccineCard/>
       </div>
       
       <SupervisionCard/>
 
       <Navbar/>
+
+      <Header/>
+      <GraphCard/>
+      <div className='flex justify-center'>
+        <WeightCard/>
+        <IccCard/>
+      </div>
+      
 
     </>
   )
---
+++ medika/frontend/src/components/icc_card/index.jsx
@@ -0,0 +1,34 @@
+import Typography from "../common/typography";
+
+function IccCard({ icc }) {
+    icc = "9"
+
+    return <>
+        <div className=" m-2">
+            <div className="flex flex-col border-2 gap-2 rounded-lg">
+                <div className="flex flex-col items-center justify-center">
+                    <Typography variant="primary" tag="h2"><h2>ICC</h2></Typography>
+                    <Typography variant="black"><p>Indice de Condition Corporelle</p></Typography>
+                </div>
+                <div className="flex flex-col items-center">
+                    <div className="flex ">
+
+                    </div>
+                    <div className="">
+                        <p><Typography tag="h2">{icc}</Typography></p>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+            <button className="flex items-center rounded-full gap-2 p-2 border-orange-500">
+                    <i class="fa-solid fa-plus"></i>
+                    <Typography>
+                        Mettre a jour le poid
+                    </Typography>
+                </button>
+        </div>
+    </>
+  }
+  export default IccCard;
+  
