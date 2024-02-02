c26be54c0a56b7dcd729956a5bf207206070b434
alex_fraioli <alex.fraioli.af@gmail.com>
2024-02-02 14:20:49 +0100

    ajout form addAnimal modif css sur les components

--- medika/frontend/src/components/animal_card/index.jsx
+++ medika/frontend/src/components/animal_card/index.jsx
@@ -1,37 +1,41 @@
+import Typography from "../common/typography"
+
 function AnimalCard({ name, birthday, breed, gender }) {
     name = 'Luna'
     birthday = "1 dec 1999"
     breed = "wolf"
     gender = ':)'
     return <>
-        <div className="flex justify-center">
-            <div className="flex flex-col items-center justify-center border-2 rounded-lg p-2 m-2">
-                <img src="img/doge.jpg" width="100px" alt="img" className="rounded-full"/>
-                <h2>{name}</h2>
-                <div className="flex items-center gap-4">
-                    <div className="birthday flex">
-                        <i className="fa-solid fa-cake-candles"></i>
-                        {birthday}
+        <div className="flex flex-col items-center relative">
+            <div className="bg-primary w-80 h-32 rounded-b-3xl absolute z-0"></div>
+            <div className="flex flex-col items-center justify-center rounded-lg p-2 m-2 z-10 mt-14 pt-10 bg-white">
+                <img src="img/doge.jpg" width="80px" alt="img" className="rounded-full absolute inset-x-2/4 transform -translate-x-1/2 -top-6 translate-y-1/2"/>
+                <h2><Typography tag="h3">{name}</Typography></h2>
+                <div className="flex items-center divide-x-2 divide-black p-4">
+                    <div className="birthday flex items-center p-2 gap-2">
+                        <i className="fa-solid fa-cake-candles text-primary"></i>
+                        <Typography variant="black">{birthday}</Typography>
                     </div>
-                    <div className="breed flex">
-                        <i className="fa-solid fa-paw"></i>
-                        {breed}
+                    <div className="breed flex items-center p-2 gap-2">
+                        <i className="fa-solid fa-paw text-primary"></i>
+                        <Typography variant="black">{breed}</Typography>
                     </div>
-                    <div className="gender flex">
-                        <i className="fa-solid fa-venus-mars"></i>
-                        {gender}
+                    <div className="gender flex items-center p-2 gap-2">
+                        <i className="fa-solid fa-venus-mars text-primary"></i>
+                        <Typography variant="black">{gender}</Typography>
                     </div>
                 </div>
-                <div className="flex bg-red-200 rounded p-2 gap-2 items-center">
+                <div className="flex bg-accentuary rounded p-2 gap-2 items-center">
                     <i className="fa-solid fa-share"></i>
-                    <button>Share</button>  
+                    <button><Typography variant="white">Share</Typography></button>  
                 </div>
             </div>
             
-            
         </div>
-
-        <p>Les donée de {name}</p>
+        <div className="flex justify-center">
+            <p>Les donée de {name}</p>
+        </div>
+        
 
 
     </>
