12549243fb2f4ad7a1a9f71682fb495844dbd34e
VChiofalo <117775290+VChiofalo@users.noreply.github.com>
2024-02-02 16:48:59 +0100

    Modification routes

--- medika/backend/app/routes.js
+++ medika/backend/app/routes.js
@@ -18,30 +18,16 @@ export default (app) => {
      const userRepository = new UserRepository(pool.promise());
      const userController = new UserController(userRepository);
      
-     app.post('/register', (req, res) => registerController.registerUserProcess(req, res));
-     app.post('/login', (req, res) => authenticationController.loginProcess(req, res));
-     app.post('/animals', (req, res) => animalController.addAnimal(req, res));
-     app.post('/users', (req, res) => userController.addUser(req, res));
-     app.get('/animals/:id', (req, res) => animalController.getAnimalById(req, res));
-     app.put('/animals/:id', (req, res) => animalController.updateAnimalById(req, res));
-     app.delete('/animals/:id', (req, res) => animalController.deleteAnimalById(req, res));
-     app.get('/users', (req, res) => userController.getAllUsers(req, res));
-     app.get('/users/:id', (req, res) => userController.getUserById(req, res));
-     app.put('/users/:id', (req, res) => userController.updateUserById(req, res));
-     app.delete('/users/:id', (req, res) => userController.deleteUserById(req, res));
-
+     app.post('/api/register', (req, res) => registerController.registerUserProcess(req, res));
+     app.post('/api/login', (req, res) => authenticationController.loginProcess(req, res));
+     app.post('/api/animals', (req, res) => animalController.addAnimal(req, res));
+     app.post('/api/users', (req, res) => userController.addUser(req, res));
+     app.get('/api/animals/:id', (req, res) => animalController.getAnimalById(req, res));
+     app.put('/api/animals/:id', (req, res) => animalController.updateAnimalById(req, res));
+     app.delete('/api/animals/:id', (req, res) => animalController.deleteAnimalById(req, res));
+     app.get('/api/users', (req, res) => userController.getAllUsers(req, res));
+     app.get('/api/users/:id', (req, res) => userController.getUserById(req, res));
+     app.put('/api/users/:id', (req, res) => userController.updateUserById(req, res));
+     app.delete('/api/users/:id', (req, res) => userController.deleteUserById(req, res));
 };
-/*const express = require('express');
-const router = express.Router();
-const ConnectionController = require('../src/controllers/ConnectionController/ConnectionController);
-const ConnectionController = new ConnectionController();
-
-router.get('/', (req, res) => res.redirect('/auth'));
-
-router.get('/')
-
-router.get('/auth', ConnectionController.get.bind(ConnectionController));
-router.post('/auth', ConnectionController.post.bind(ConnectionController));
-
-module.exports = router;*/
 
