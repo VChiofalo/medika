148c8e3141cc66b3b2a102ab074b0b0152282ce1
Fredericvamer <frederic.vamer@epsi.fr>
2024-02-05 09:33:16 +0100

    css sur loginForm et registerForm

--- medika/frontend/src/components/forms/LoginPage.jsx
+++ medika/frontend/src/components/forms/LoginPage.jsx
@@ -1,72 +1,109 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-class LoginPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            email: '',
-            password: ''
-        };
-    }
+import { useRef, useState } from 'react';
 
-    handleEmailChange = (event) => {
-        this.setState({ email: event.target.value });
-    };
+const SignupForm = () => {
+    const formRef = useRef();
+    const [error, setError] = useState(null);
 
-    handlePasswordChange = (event) => {
-        this.setState({ password: event.target.value });
-    };
-
-    handleSubmit = (event) => {
+    const handleSubmit = async (event) => {
         event.preventDefault();
-        console.log('Email:', this.state.email);
-        console.log('Password:', this.state.password);
+
+        const data = new FormData(formRef.current);
+        const firstname = data.get('firstname');
+        const lastname = data.get('lastname');
+        const email = data.get('email');
+        const password = data.get('password');
+        const confirmpassword = data.get('confirmPassword');
+
+        if (password !== confirmpassword) {
+            setError('Les mots de passe ne sont pas identiques');
+            // clear password fields in dom
+            const formField = formRef.current.querySelectorAll('input[type="password"]');
+            formField.forEach(field => field.value = '');
+            return;
+        }
+
+        const body = {
+            firstname,
+            lastname,
+            email,
+            password
+        }
+
+        console.log(body);
+        const request = await fetch('http://localhost:3000/register', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+                Accept: 'application/json',
+            },
+            body: JSON.stringify(body)
+        })
+        const response = await request.json();
+        console.log(response)
     };
 
-    handleSignup = (event) => {
-        event.preventDefault();
-        // Logique pour rediriger l'utilisateur vers la page d'inscription ou effectuer d'autres actions
-        console.log("Redirection vers la page d'inscription...");
-    };
-
-    render() {
-        return (
-            <form onSubmit={this.handleSubmit}>
-                <div>
-                    <label htmlFor="email">Email:</label>
+    return (
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh', marginTop: '50px' }}>
+            <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '20px', width: '60%', height: '60px', backgroundColor: '#6FCF97', borderRadius: '10px' }}>
+                <img src="/images/Medika.png" alt="Main Logo" style={{ width: '90px', height: '25px', marginRight: '0px' }} />
+                <img src="/images/Frame18.png" alt="Second Logo" style={{ width: '20px', height: '20px', marginLeft: '0px', marginBottom: '5px' }} />
+            </div>
+            <form ref={formRef} onSubmit={handleSubmit} style={{
+                width: '300px',
+                padding: '20px',
+                border: '1px solid #ccc',
+                borderRadius: '5px',
+                display: 'flex',
+                flexDirection: 'column',
+                alignItems: 'center',
+            }}>
+    
+                <div style={{ marginBottom: '10px' }}>
                     <input
                         name="email"
                         type="email"
                         id="email"
-                        value={this.state.email}
-                        onChange={this.handleEmailChange}
                         required
+                        placeholder="Email"
+                        style={{ marginBottom: '5px' }}
                     />
+                    <hr style={{ margin: '0' }} />
                 </div>
-                <div>
-                    <label htmlFor="password">Mot de passe:</label>
+                {error && <p>{error}</p>}
+                <div style={{ marginBottom: '10px' }}>
                     <input
                         name="password"
                         type="password"
                         id="password"
-                        value={this.state.password}
-                        onChange={this.handlePasswordChange}
                         required
+                        placeholder="Mot de passe"
+                        style={{ marginBottom: '5px' }}
                     />
+                    <hr style={{ margin: '0' }} />
                 </div>
-                <div>
-                    <button type="submit">Se connecter</button>
-                    <br /> {/* Ajout d'un saut de ligne*/}
-                    <div className="continue-button bg-green-500 text-white py-3 px-8 rounded-full text-lg font-semibold shadow-lg">
-                        <Link to="/register">Inscription</Link>
-                    </div>
-                    
-                    <button onClick={this.handleSignup}>Inscription</button>
-                    
-                </div>
+      
+                <button type="submit" style={{ 
+    backgroundColor: '#FF7F00', 
+    color: '#ffffff',
+    border: 'none',
+    borderRadius: '20px',
+    padding: '8px 20px',
+    cursor: 'pointer',
+    fontSize: '18px',
+    marginTop: '10px',
+    transition: 'background-color 0.3s ease',
+    width: '180px',
+    height: '40px',
+    textAlign: 'center' 
+    }}>Connexion</button>
+    
             </form>
-        );
-    }
+        </div>
+    );
+
+       
 }
 
-export default LoginPage;
+export default SignupForm;
+
+
