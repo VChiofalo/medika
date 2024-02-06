a20622253abb96b5dbdd5096f3381d36d9994430
Chant76 <mariemeaidara66@gmail.com>
2024-02-06 16:12:53 +0100

    weight_card

--- medika/frontend/src/components/cards/icc_card/index.jsx
+++ medika/frontend/src/components/cards/icc_card/index.jsx
@@ -1,32 +1,35 @@
 import Typography from "../../common/typography";
 
 function IccCard({ icc }) {
-    icc = "9"
+    icc = "4.5"
 
     return <>
-        <div className=" m-2">
-            <div className="flex flex-col border-2 gap-2 rounded-lg">
-                <div className="flex flex-col items-center justify-center">
-                    <Typography variant="primary" tag="h2"><h2>ICC</h2></Typography>
-                    <Typography variant="black"><p>Indice de Condition Corporelle</p></Typography>
-                </div>
-                <div className="flex flex-col items-center">
-                    <div className="flex ">
-
-                    </div>
+        <div className=" m-4">
+            <div className="flex flex-col justify-center w-60 h-50  bg-white shadow-2xl rounded-3xl m-2 p-10">
+                <div className="border-20 bg-white  items-center justify-center ">
                     <div className="">
-                        <p><Typography tag="h2">{icc}</Typography></p>
+                        <Typography variant="primary" tag="h1"><h2  className="text-center text-2xl font-bold text-gray-900">ICC</h2></Typography>
+                        <Typography variant="black"><p className="text-center text-gray-500 text-sm">Indice de Condition Corporelle</p></Typography>
                     </div>
-                    
-                    
+                    <div className="flex flex-col items-center">
+                        <div className="">
+                            <p><Typography tag="h2"><h2 className="text-primary ">{icc}</h2></Typography></p>
+                        </div>
+                        
+                        
+                    </div>
                 </div>
             </div>
-            <button className="flex items-center rounded-full gap-2 p-2 border-orange-500">
-                    <i class="fa-solid fa-plus"></i>
-                    <Typography>
-                        Mettre a jour le poid
+            <button className="flex items-center group inline-block w-60 justify-center items-center rounded-full bg-white gap-2 p-2 border-accentuary">
+                    <i className="fa-solid fa-plus text-orange-500"></i>
+                    <Typography ><p className="text-accentuary ">
+                    Mettre a jour le poid
+                    </p>
+                       
                     </Typography>
-                </button>
+             </button>
+             <br />
+            
         </div>
     </>
   }
--- medika/frontend/src/components/cards/weight_card/index.jsx
+++ medika/frontend/src/components/cards/weight_card/index.jsx
@@ -1,23 +1,23 @@
 import Typography from "../../common/typography";
 
 function WeightCard({ weight, previous_weight }) {
-    weight = "12"
+    weight = "12.5"
     previous_weight = "15"
     let actualWeight = weight - previous_weight
 
     return <>
-        <div className="flex flex-col justify-center">
-            <div className="border-2 flex-col items-center justify-center rounded-lg m-2 h-full ">
+        <div className="flex flex-col justify-center w-60 h-60 m-6 bg-white shadow-2xl rounded-3xl m-2 p-10 ">
+            <div className="border-20 bg-white  items-center justify-center  ">
                 <div className=" ">
-                    <Typography variant="primary" tag="h2"><h2>Estimation</h2></Typography>
-                    <Typography variant="black"><p>pour le mois prochain</p></Typography>
+                    <Typography variant="primary" tag="h1"><h2 className="text-center text-2xl font-bold text-gray-900">Estimation</h2></Typography>
+                    <Typography variant="black"><p className="text-center text-gray-500 text-sm">pour le mois prochain</p></Typography>
                 </div>
                 <div className="">
-                    <div className=" ">
-                        <p><Typography>{actualWeight}</Typography></p>
+                    <div className="flex justify-center ">
+                        <p ><Typography>{actualWeight}kg</Typography></p>
                     </div>
                     <div className="">
-                        <p><Typography tag="h3">{weight} kg</Typography></p>
+                        <p className="text-center "><Typography tag="h2"><h2 className="text-primary ">{weight} kg</h2></Typography></p>
                     </div>
                     
                     
