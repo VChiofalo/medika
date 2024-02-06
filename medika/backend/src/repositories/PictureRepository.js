b3e9ed7ab1702671b512603bdec92e9de10e7250
VChiofalo <117775290+VChiofalo@users.noreply.github.com>
2024-02-06 17:28:37 +0100

    A finir

--- medika/backend/src/repositories/PictureRepository.js
+++ medika/backend/src/repositories/PictureRepository.js
@@ -1,10 +1,14 @@
 import connection from '../../app/database_sql.js';
 
 export default class PictureRepository{
-    async addUserPicture(slug, user_email){
-        console.log(slug, user_email);
-        const request = await connection.promise().query('INSERT INTO `photos` SET ?', {slug, user_email});
-        console.log(request);
+    async addUserPicture(datas){
+        console.log(datas.slug);
+        //console.log(user_email)
+
+        await connection.promise().query('INSERT INTO `photos` (slug, user_email) VALUES (?, ?)', datas.slug, datas.user_email);
+        //console.log('--')
+        //console.log(request);
+        //console.log('--')
     }
 
     async addAnimalPicture(slug, id_animals){
