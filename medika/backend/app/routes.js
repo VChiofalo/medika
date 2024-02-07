7538db1074ef5bc87bbcfc24cd08da67b3aa5dbc
Erwan <128072355+Veleoran@users.noreply.github.com>
2024-02-07 12:49:51 +0100

    Erwan animal (#46)
    * Mise à jour des méthodes dans AnimalController
    
    * integration jwstoken back pour animals and user email
    
    * integration jwstoken back pour animals and user email
    
    ---------
    
    Co-authored-by: VChiofalo <117775290+VChiofalo@users.noreply.github.com>

--- medika/backend/app/routes.js
+++ medika/backend/app/routes.js
@@ -1,5 +1,6 @@
+import authenticateJWT from "../src/middleware/authenticateJWT.js";
 import RegisterController from "../src/controllers/RegisterController/RegisterController.js";
-import AuthenticationController from '../src/controllers/AuthenticationController/AuthenticationController.js';
+import AuthenticationController from "../src/controllers/AuthenticationController/AuthenticationController.js";
 // import UserController from "../src/controllers/UserController/UserController.js";
 import AnimalController from "../src/controllers/AnimalController/AnimalController.js"
 import PictureController from "../src/controllers/PictureController/PictureController.js";
@@ -16,7 +17,7 @@ export default (app) => {
      app.post('/login', (req, res) => authenticationController.loginProcess(req, res));
      
      app.use('/api', userExists);
-     
+
      app.post('/api/animals', (req, res) => animalController.addAnimal(req, res));
      app.get('/api/animals', (req, res) => animalController.getAnimalById(req, res));
      app.put('/api/animals', (req, res) => animalController.updateAnimalById(req, res));
@@ -26,5 +27,6 @@ export default (app) => {
      app.post('/api/animal/picture', upload, (req, res) => pictureController.addAnimalPictureProcess(req, res));
      app.get('/api/user/pictures', (req, res) => pictureController.getUserPicturesProcess(req, res));
      app.get('/api/animal/pictures', (req, res) => pictureController.getAnimalPicturesProcess(req, res));
+
 };
 
