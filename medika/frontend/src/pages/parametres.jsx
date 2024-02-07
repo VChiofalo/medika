7619729a16fa4e9736d313c187310a203b63426f
Fredericvamer <frederic.vamer@epsi.fr>
2024-02-07 16:19:29 +0100

    css logout parametres

--- medika/frontend/src/pages/parametres.jsx
+++ medika/frontend/src/pages/parametres.jsx
@@ -1,46 +1,47 @@
-import { Link } from "react-router-dom";
-import Typography from "../components/common/typography";
-import { useDispatch } from "react-redux";
-import { logout } from "../store/slices/userSlice";
-import { useState } from "react";
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import Typography from '../components/common/typography';
+import { useDispatch } from 'react-redux';
+import { logout } from '../store/slices/userSlice';
 
 const Parameter = () => {
-    const dispatch = useDispatch();
-    const [deconnexionEffectuee, setDeconnexionEffectuee] = useState(false);
+  const dispatch = useDispatch();
+  const [deconnexionEffectuee, setDeconnexionEffectuee] = useState(false);
 
-    const handleLogout = () => {
-        localStorage.clear();
-        dispatch(logout());
-        setDeconnexionEffectuee(true);
-        setTimeout(() => {
-            setDeconnexionEffectuee(false);
-        }, 2000); 
-    };
+  const handleLogout = () => {
+    localStorage.clear();
+    dispatch(logout());
+    setDeconnexionEffectuee(true);
+    setTimeout(() => {
+      setDeconnexionEffectuee(false);
+    }, 2000);
+  };
 
-    return (
-        <div>
-            {deconnexionEffectuee && (
-                <div style={{ backgroundColor: '#f0f0f0', padding: '10px', marginBottom: '10px' }}>Vous êtes déconnecté.</div>
-            )}
-            <div>
-                <div className="button-carousel"></div>
-                <Link
-                    onClick={handleLogout}
-                    to="/"
-                    className="flex items-center rounded-full bg-white p-1 px-2 gap-2"
-                >
-                    <i className="fa-solid fa-user"></i>
-                    
-                    <Typography tag={"p"} customClasses={"mt-4 text-center lg:text-base"}>
-          Mettre fin à la session ?{" "}
-          <Link to="/logout" className="text-primary">
-            Deconnexion
-          </Link>
-        </Typography>
-
-                    
-                </Link>
-            </div>
+  return (
+    <div className="bg-white min-h-screen">
+      <div
+        style={{
+          marginBottom: '20px',
+          textAlign: 'center',
+        }}
+        className="bg-primary py-6 rounded-b-lg"
+      >
+        <div className="flex justify-center">
+          <img
+            src="/images/Medika.png"
+            alt="Main Logo"
+            style={{ width: '90px', height: '25px', marginRight: '0px' }}
+          />
+          <img
+            src="/images/Frame18.png"
+            alt="Second Logo"
+            style={{
+              width: '20px',
+              height: '20px',
+              marginLeft: '0px',
+              marginBottom: '5px',
+            }}
+          />
         </div>
       </div>
       <div
