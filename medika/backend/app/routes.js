8af6564dbd9d4e5789bd93480dedfc37dbe9526c
Veleoran <erwan.mazeas.14.17@gmail.com>
2024-02-07 14:43:52 +0100

    back formulaire animal final

--- medika/backend/app/routes.js
+++ medika/backend/app/routes.js
@@ -1,4 +1,4 @@
-import authenticateJWT from "../src/middleware/authenticateJWT.js";
+// import authenticateJWT from "../src/middleware/authenticateJWT.js";
 import RegisterController from "../src/controllers/RegisterController/RegisterController.js";
 import AuthenticationController from "../src/controllers/AuthenticationController/AuthenticationController.js";
 // import UserController from "../src/controllers/UserController/UserController.js";
@@ -18,7 +18,7 @@ export default (app) => {
      
      app.use('/api', userExists);
 
-     app.post('/api/animals', (req, res) => animalController.addAnimal(req, res));
+     app.post('/api/add_animals', (req, res) => animalController.addAnimal(req, res));
      app.get('/api/animals', (req, res) => animalController.getAnimalById(req, res));
      app.put('/api/animals', (req, res) => animalController.updateAnimalById(req, res));
      app.delete('/api/animals', (req, res) => animalController.deleteAnimalById(req, res));
